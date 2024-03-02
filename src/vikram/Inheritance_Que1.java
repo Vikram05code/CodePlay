22932d0c939cabe69d215d17f62a72cdf6ee9931
Vikram <vikky5vikram@gmail.com>
2024-03-03 01:33:16 +0530

    Initial Commit for CodePlay Repository

---
+++ src/vikram/Inheritance_Que1.java
@@ -0,0 +1,14 @@
+package vikram;
+
+//Write a Java program demonstrating inheritance where you have a superclass Shape and subclasses Circle and Rectangle. Implement methods to calculate area and perimeter for each shape.
+
+public class Inheritance_Que1 {
+
+	public int area() {
+		
+		
+		
+		return 0;
+	}
+	
+}
