f712617c3c862cfd3c7c2e3f440478ae283158ca
Vikram <vikky5vikram@gmail.com>
2024-03-19 15:49:19 +0530

    Binary Search in Array

---
+++ src/com/vikram/BinarySearch.java
@@ -0,0 +1,44 @@
+package com.vikram;
+
+public class BinarySearch {
+
+	public static void main(String[] args) {
+		
+		int[] nums = {5, 8, 12, 14, 16, 20, 22, 24,25};
+		int target = 22;
+		int result = binarySearching(nums, target);
+		
+		if(result != -1) {
+			System.out.println("Element found at index : "+ result);
+		}
+		else {
+			System.out.println("Element not found");
+		}
+		
+	}
+
+	public static int binarySearching(int[] nums, int target) {
+	
+		int left = 0;
+		int right = nums.length-1;
+		
+		while(left<=right) {
+			
+			int mid = (left+right)/2;
+			
+			if(nums[mid]==target) {
+				return mid;
+			}
+			else if(mid>target) {
+				right=mid-1;
+			}
+			else {
+				left=mid+1;
+			}
+			
+		}
+		return -1;
+	}
+	
+	
+}
