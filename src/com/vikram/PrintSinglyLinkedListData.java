d8c7b85529f88c9ad3408e31a770b21876adf64a
Vikram <vikky5vikram@gmail.com>
2024-07-01 23:51:29 +0530

    PrintSinglyLinkedListData

--- src/com/vikram/PrintSinglyLinkedListData.java
+++ src/com/vikram/PrintSinglyLinkedListData.java
@@ -1,8 +1,9 @@
 package com.vikram;
 
-public class SinglyLinkedList {
 
-	private ListNode head;
+public class PrintSinglyLinkedListData {
+
+     private ListNode head;
 	
 	private static class ListNode{
 		
@@ -17,20 +18,35 @@ public class SinglyLinkedList {
 		
 	}
 	
+	public void display() {
+		ListNode current = head;
+		while(current != null) {
+			System.out.print(current.data+" --> ");
+			current=current.next;
+		}
+		System.out.print("null");
+	}
+	
 	public static void main(String[] args) {
 		
-		SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
-		singlyLinkedList.head=new ListNode(12);
+		PrintSinglyLinkedListData printSinglyLinkedListData = new PrintSinglyLinkedListData();
+		printSinglyLinkedListData.head=new ListNode(12);
 		ListNode second = new ListNode(10);
 		ListNode third = new ListNode(5);
 		ListNode fourth = new ListNode(2);
 		
 		//now connect node to each other
 		
-	singlyLinkedList.head.next=second;  //12-->10
+	printSinglyLinkedListData.head.next=second;  //12-->10
 	second.next=third;     //12-->10-->5
 	third.next=fourth;     //12-->10-->5-->2
 	fourth.next=null;      //12-->10-->5-->2-->null;	
+	
+	printSinglyLinkedListData.display();
+	
 	}
+	
+	
+	
 	
 }
