3967b563498e878835a46bfb48437bf61cd925bc
Vikram <vikky5vikram@gmail.com>
2024-07-28 21:18:03 +0530

    Swap Numbers Without Using Third Variable

--- src/com/vikram/SwapNumbersWithoutUsingThirdVariable.java
+++ src/com/vikram/SwapNumbersWithoutUsingThirdVariable.java
@@ -1,26 +1,26 @@
 package com.vikram;
 
-public class SwapTwoNumber {
+public class SwapNumbersWithoutUsingThirdVariable {
 	
 	public static void main(String[] args) {
 		
 		int a=10;
 		int b=20;
-		
-		swapNumber(a, b);
-		
+		swapNumberWithoutThirdVariable(a, b);
 		
 	}
 	
-	public static void  swapNumber(int a, int b) {
+ public static void swapNumberWithoutThirdVariable(int a, int b) {
 		
 		System.out.println("Before swapping:  "+"a="+a+", b="+b);
-		int temp=0;
-		temp=a;
-		a=b;
-		b=temp;
+		
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		
 		System.out.println("After swapping:  "+"a="+a+", b="+b);
+		
+		
 	}
-	
 
 }
