58da46badefcbcc2f04f1cb2d1d017db2cceccd2
Vikram <vikky5vikram@gmail.com>
2024-11-01 19:36:44 +0530

    Separate Odd even Number using Java 8 features

---
+++ src/com/vikram/SeparateOddAndEven.java
@@ -0,0 +1,36 @@
+package com.vikram;
+//Given a list of integers, separate odd and even numbers?
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.Map.Entry;
+
+public class SeparateOddAndEven {
+	
+	public static void main(String[] args) {
+		
+		List<Integer> listOfIntegers = Arrays.asList(71, 18, 42, 21, 67, 32, 95, 14, 56, 87);
+		Map<Boolean, List<Integer>> oddEvenNumMap = listOfIntegers.stream().collect(Collectors.partitioningBy(i->i%2==0));
+		Set<Entry<Boolean, List<Integer>>> entrySet = oddEvenNumMap.entrySet();
+		
+		for(Entry<Boolean, List<Integer>>  entry : entrySet) {
+			System.out.println("---------------------------------------------------------------------");
+			if(entry.getKey()) {
+				System.out.println("It's a even number");
+			}else {
+				System.out.println("It's a odd number");
+			}
+			System.out.println("----------------------------------------------------------------------");
+			List<Integer> list = entry.getValue();
+			
+			for(int i : list) {
+				System.out.println(i);
+			}
+		}
+		
+		
+	}
+
+}
