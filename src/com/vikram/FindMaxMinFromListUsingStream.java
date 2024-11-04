0ac9b19dac2c9ebcb35383e1af42afb220e3b179
Vikram <vikky5vikram@gmail.com>
2024-11-04 22:21:50 +0530

    Given a list of integers, find maximum and minimum of those numbers?

---
+++ src/com/vikram/FindMaxMinFromListUsingStream.java
@@ -0,0 +1,17 @@
+package com.vikram;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class FindMaxMinFromListUsingStream {
+	
+	public static void main(String[] args) {
+		List<Integer> numberList = Arrays.asList(23,15,28,13,29,10,40,45,18,5,55,2);
+		Integer max = numberList.stream().max(Comparator.naturalOrder()).get();
+		Integer min = numberList.stream().min(Comparator.naturalOrder()).get();
+		System.out.println("Maximum Number in Given Number List is "+max);
+		System.out.println("Minimum Number in Given Number List is "+min);
+	}
+
+}
