b268b2bdefdc0cf2e6445053da3c6c62b4f08bbf
Vikram <vikky5vikram@gmail.com>
2024-12-22 10:51:48 +0530

    How to insert at first postion and last position of LinkedList

---
+++ src/com/vikram/linkedList/Main.java
@@ -0,0 +1,18 @@
+package com.vikram.linkedList;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		LinkedList1 linkedList1 = new LinkedList1();
+		linkedList1.insertFirst(12);
+		linkedList1.insertFirst(8);
+		linkedList1.insertFirst(16);
+		linkedList1.insertFirst(15);
+		linkedList1.insertFirst(4);
+		linkedList1.insertFirst(20);
+		linkedList1.insertLast(80);
+		
+		linkedList1.display();
+	}
+
+}
