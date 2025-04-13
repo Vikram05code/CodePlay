a15928ff3b4b514d5444ba0a0e0bd8879e285ecd
Vikram <vikky5vikram@gmail.com>
2025-04-13 23:06:36 +0530

    Flatten a nested list using Stream API

---
+++ src/com/vikram/FlattenNestedList.java
@@ -0,0 +1,20 @@
+package com.vikram;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FlattenNestedList {
+	
+	public static List<Integer> flatten(List<List<Integer>> nestedLists){
+		return nestedLists.stream().flatMap(List::stream).collect(Collectors.toList());
+	}
+	
+	public static void main(String[] args) {
+		List<List<Integer>> nestedLists = Arrays.asList(Arrays.asList(5,10,12,6,20),
+				Arrays.asList(55,30,2,16,40),Arrays.asList(65,60,32,76,40));
+		System.out.println(flatten(nestedLists));
+	}
+
+}
